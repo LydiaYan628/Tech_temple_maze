f0725b5898606881490e3275c3a70e19335afb80
lydia <zy1562@nyu.edu>
2021-12-10 12:50:35 +0400

    final page css need to change the documentation link at the end

--- exit/sketch.js
+++ exit/sketch.js
@@ -34,6 +34,7 @@ function gotResults(error, results) {
   console.log(said);
 }
 
+
 function gotPoses(poses) {
   //console.log(poses);
   if (poses.length > 0) {
@@ -113,3 +114,11 @@ function decision(){
     setTimeout(function(){window.location.href = '../main_temple/index.html'},3000);
   }
 }
+
+jQuery(document).ready(function(){
+   $('p').mousemove(function(e){
+     var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+     var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+     $('p').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+   });
+});
